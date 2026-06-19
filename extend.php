2c98c0e59ac29ff32b0c40af4ffc340bcc777cf7
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-06-19 16:40:10 +0200

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,19 +1,18 @@
 <?php
 
-/**
- *  This file is part of noriods/auto-more.
- *
- *  Copyright (c) 2019 .
- *
- *
- *  For the full copyright and license information, please view the LICENSE.md
- *  file that was distributed with this source code.
- */
 namespace PeopleInside\AutoMore;
 
 use Flarum\Extend;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__ . '/js/dist/forum.js'),
+        ->js(__DIR__.'/js/dist/forum.js'),
+
+    new Extend\Locales(__DIR__.'/locale'),
+
+    // Registra la preferenza utente (default: true = attivo)
+    (new Extend\User())
+        ->registerPreference('automore_enabled', function ($value) {
+            return (bool) $value;
+        }, true),
 ];
