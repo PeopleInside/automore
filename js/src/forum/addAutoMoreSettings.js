ef28c7fcd1c666222af53b2411f8b5b588b894fa
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-06-19 17:04:17 +0200

    Update addAutoMoreSettings.js

--- js/src/forum/addAutoMoreSettings.js
+++ js/src/forum/addAutoMoreSettings.js
@@ -1,32 +1,34 @@
 import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
-import SettingsPage from 'flarum/forum/components/SettingsPage';
 import Switch from 'flarum/common/components/Switch';
 
 export default function addAutoMoreSettings() {
-  // Estendi la sezione "privacy" delle impostazioni utente
-  extend(SettingsPage.prototype, 'privacyItems', function (items) {
+  // In Flarum 2.0, SettingsPage è lazy-loaded, quindi dobbiamo usare il percorso di importazione
+  extend('flarum/forum/components/SettingsPage', 'privacyItems', function (items) {
     if (!this.user) return;
 
     items.add(
       'automore',
-      <Switch
-        state={this.user.preferences()?.automore_enabled !== false}
-        onchange={(value) => {
-          this.automoreLoading = true;
-          this.user.savePreferences({ automore_enabled: value }).then(() => {
-            this.automoreLoading = false;
-            m.redraw();
-          });
-        }}
-        loading={this.automoreLoading}
-      >
-        {app.translator.trans('peopleinside-automore.forum.settings.enable_label')}
-        <span className="helpText">
+      <div className="Form-group">
+        {Switch.component(
+          {
+            state: this.user.preferences()?.automore_enabled !== false,
+            onchange: (value) => {
+              this.automoreLoading = true;
+              this.user.savePreferences({ automore_enabled: value }).then(() => {
+                this.automoreLoading = false;
+                m.redraw();
+              });
+            },
+            loading: this.automoreLoading,
+          },
+          app.translator.trans('peopleinside-automore.forum.settings.enable_label')
+        )}
+        <p className="helpText">
           {app.translator.trans('peopleinside-automore.forum.settings.enable_help')}
-        </span>
-      </Switch>,
-      50 // Priorità: appare dopo "discloseOnline" (che ha priorità 100)
+        </p>
+      </div>,
+      50 // Priorità: appare dopo le impostazioni di privacy native
     );
   });
 }
