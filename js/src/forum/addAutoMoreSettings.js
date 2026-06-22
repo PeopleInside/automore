bf1a3658a1e2b188f25a2e70c5707d190162170e
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-06-22 16:33:11 +0200

    Update addAutoMoreSettings.js

--- js/src/forum/addAutoMoreSettings.js
+++ js/src/forum/addAutoMoreSettings.js
@@ -1,9 +1,11 @@
 import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import Switch from 'flarum/common/components/Switch';
+import SettingsPage from 'flarum/forum/pages/SettingsPage';
 
 export default function addAutoMoreSettings() {
-  extend('flarum/forum/pages/SettingsPage', 'privacyItems', function (items) {
+  // Correzione: passiamo il prototipo della classe importata invece di una stringa
+  extend(SettingsPage.prototype, 'privacyItems', function (items) {
     if (!this.user) return;
 
     console.debug('[automore] Settings toggle added');
@@ -12,20 +14,24 @@ export default function addAutoMoreSettings() {
       'automore',
       <div className="Form-group">
         <h3>{app.translator.trans('peopleinside-automore.forum.settings.heading')}</h3>
-        <div>
-          {Switch.component(
-            {
-              state: this.user.preferences()?.automore_enabled !== false,
-              onchange: (value) => {
-                this.user.savePreferences({ automore_enabled: value });
-              },
+        {Switch.component(
+          {
+            state: this.user.preferences()?.automore_enabled !== false,
+            onchange: (value) => {
+              this.automoreLoading = true;
+              this.user.savePreferences({ automore_enabled: value }).then(() => {
+                this.automoreLoading = false;
+                m.redraw(); // 'm' è disponibile globalmente in Flarum
+              }).catch(() => {
+                this.automoreLoading = false;
+                m.redraw();
+              });
             },
-            app.translator.trans('peopleinside-automore.forum.settings.enable_label')
-          )}
-          <p className="helpText">
-            {app.translator.trans('peopleinside-automore.forum.settings.enable_help')}
-          </p>
-        </div>
+            loading: this.automoreLoading,
+          },
+          app.translator.trans('peopleinside-automore.forum.settings.enable_label')
+        )}
+        <p className="helpText">{app.translator.trans('peopleinside-automore.forum.settings.enable_help')}</p>
       </div>,
       50
     );
